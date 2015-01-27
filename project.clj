0a60fd8990fee92387dcae0d8f7a0a51b0bdc608
Roger Goldfinger <rgoldfinger@gmail.com>
2015-01-26 19:00:49 -0800

    fix dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.3.2"]
+                 [compojure "1.3.1"]
                  [ring/ring-json "0.1.2"]
                  [c3p0/c3p0 "0.9.1.2"]
                  [http-kit "2.1.16"]
