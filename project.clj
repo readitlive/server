8b080018746d00a08741b050abd8c662ff61cd7e
Roger Goldfinger <rgoldfinger@gmail.com>
2015-01-26 18:59:06 -0800

    update dependencies

--- project.clj
+++ project.clj
@@ -3,10 +3,11 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.3.1"]
+                 [compojure "1.3.2"]
                  [ring/ring-json "0.1.2"]
                  [c3p0/c3p0 "0.9.1.2"]
                  [http-kit "2.1.16"]
+                 [javax.servlet/servlet-api "2.5"]
                  [cheshire "4.0.3"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler sir.handler/app}
